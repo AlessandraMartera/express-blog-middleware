0b0dbc36bbc29b9fd90189324157d07d4520131c
alessandra martera <ale.martera@gmail.com>
2023-11-15 18:12:09 +0100

    update

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,13 @@
 // collego il json dei posts dentro la carte db a post.js
 const fs = require("fs");
-const posts = require("../db/db.json");
+const users  = require("../db/user.json");
 const path = require("path");
+// importo la funzione per generare io token
+const generateJWT = require("../middlewares/jwtToken");
 
-// INDEx
+// Login
 module.exports.login = function(req, res) {
-    res.end("rotta per fare il login")
+    const { ursername, password } = req.body
+
+    const user = posts.find( user => user.ursername === ursername && user)
 }
